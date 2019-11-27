a8d49f2325bd7fb459c15babef0b9c414c128946
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-27 05:48:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,14 +24,15 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
    holiday_hash[:winter].each do |season, decorations|
      decorations << supply  
-    
-    
-  end
+   end
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+   holiday_hash[:spring].each do |season, decorations|
+     decorations << supply
+
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
