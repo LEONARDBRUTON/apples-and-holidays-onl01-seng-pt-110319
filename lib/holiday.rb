e9fc150fe8ee64562c3f192ac7743810d0e353b3
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-27 05:59:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,13 +38,11 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
-  # code here
-  # remember to return the updated hash
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter]
 
 end
 
