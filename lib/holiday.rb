9b1763d0b2616ce90f3ce3c48f4fcb91824dc4a4
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-27 06:02:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter]
+  holiday_hash[:winter].map do |season, supply|
 
 end
 
