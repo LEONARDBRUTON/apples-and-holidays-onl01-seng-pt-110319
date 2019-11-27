3603a0bab41eef02c3df91a58dae98d845867232
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-27 06:18:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts holiday_hash
+    
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
